2f8e03c277e1d223ba427d9826bb483c80da9406
Ilja Zaglov <ilja@zaglov.de>
2014-03-31 17:34:09 +0200

    templateServer aktualisiert
    Unnötige zum Testen benötigte Codezeilen entfernt

--- templateServer.php
+++ templateServer.php
@@ -4,16 +4,6 @@ require_once("src/TEMPY.CLASS.php");
 
 header("Content-type: text/json");
 
-
-
-/*$_POST['arguments'] = array(
-
-    array("name" => "mainView"),
-    array("name" => "template2")
-
-);*/
-
-//$_POST['args'][0]['name'] = "mainView";
 $tempy = new TEMPY();
 
 foreach($_POST['arguments'] as $template){
@@ -25,21 +15,5 @@ $tempy -> getTemplates();
 
 
 
-/*
-//print_r($_POST);
 
-
-// Templates einlesen
-// Template Packs einlesen
-// Templates Ausgeben
-// Forced Expiration
-
-echo json_encode(
-        array(
-            array(  "templateName" => $_POST['args'][0]['name'],
-                    "templateContent" => file_get_contents("template1.tpl")
-            )
-        )
-    );
-*/
 ?>
